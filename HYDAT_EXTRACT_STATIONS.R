477c307b5168dfc6f6bdd62c103ee5fa3856c217
Francois Robinne <robinne@ualberta.ca>
2020-07-20 14:32:11 -0600

    Formating updates (Tidyverse style)

--- HYDAT_EXTRACT_STATIONS.R
+++ HYDAT_EXTRACT_STATIONS.R
@@ -3,83 +3,89 @@
 ## Description:
 ## Project: "CANADA_INVENTORY-HYDRO-FIRE-DATA"
 ## Date: 2020-07-09
-## Auteur: Francois-Nicolas Robinne
+## Author: Francois-Nicolas Robinne
 ## Contact: robinne@ualberta.ca
-## Licence: GNU General Public License
-## Notes: 
+## License: MIT
+## Notes:
 ############
 
 # Main code ---------------------------------------------------------------
 # Libraries -------------------
-  library(tidyhydat)
-  library(dplyr)
-  library(ggplot2)
-  library(sf)
+library(tidyhydat)
+library(dplyr)
+library(ggplot2)
+library(sf)
 
 # Working directory ----------------
-  setwd("D:/PROJECTS/21_2018_CANADA_INVENTORY-HYDRO-FIRE-DATA_ACTIVE") # For user to change as necessary
+setwd("D:/PROJECTS/21_2018_CANADA_INVENTORY-HYDRO-FIRE-DATA_ACTIVE") # For user to change as necessary
 
 # Load data -------------------------------------------------------------
-  wcs <- st_read("02_TRANSFORMED_DATA/NRCAN/WSC_Watersheds_2015.shp")
-  nfdb <- st_read("01_RAW_DATA/CWFIS/NFDB_poly_20190607.shp")
+watersheds_wcs <- st_read("02_TRANSFORMED_DATA/NRCAN/WSC_Watersheds_2015.shp")
+fires_nfdb <- st_read("01_RAW_DATA/CWFIS/NFDB_poly_20190607.shp")
 
 # Extract HYDAT gauges ---------------
 # This part extracts the location (LAT,LON) of gauges active in 1980
-  stns_1980_list <- hy_annual_stats(start_year = 1980, end_year = 1980) %>%
-    select(STATION_NUMBER, Parameter, Year) %>%
-    filter(Parameter == "Flow") %>% # Only keeping one parameter to limit station redudancy
-    pull_station_number() %>%
-    hy_stations() %>%
-    select(STATION_NUMBER, STATION_NAME, DRAINAGE_AREA_GROSS, LATITUDE, LONGITUDE) %>% # Will remove the info on drainage area when I have the watersheds
-    st_as_sf(coords = c("LONGITUDE", "LATITUDE"), crs = 4326)
-    
-  ggplot() +
-    geom_sf(data = stns_1980_list) # Plot point layer showing stations in 1980
-  st_write(stns_1980_list, "02_TRANSFORMED_DATA/HYDAT/stns_1980_location.shp", append = F) # Write point shapefile
-  
+stns_1980_list <- hy_annual_stats(start_year = 1980, end_year = 1980) %>%
+  select(STATION_NUMBER, Parameter, Year) %>%
+  filter(Parameter == "Flow") %>% # Only keeping one parameter to limit station redudancy
+  pull_station_number() %>%
+  hy_stations() %>%
+  select(STATION_NUMBER, STATION_NAME, DRAINAGE_AREA_GROSS, LATITUDE, LONGITUDE) %>% # Will remove the info on drainage area when I have the watersheds
+  st_as_sf(coords = c("LONGITUDE", "LATITUDE"), crs = 4326)
+
+ggplot() +
+  geom_sf(data = stns_1980_list) # Plot point layer showing stations in 1980
+st_write(stns_1980_list, "02_TRANSFORMED_DATA/HYDAT/stns_1980_location.shp", append = F) # Write point shapefile
+
 # This part extracts the location (LAT,LON) of gauges active between 1981 and 2019
-# This part extracts the location (LAT,LON) of gauges active between 1981 and 2019
-  stns_1981_2019_list <- hy_annual_stats(start_year = 1981, end_year = 2019) %>%
-    select(STATION_NUMBER, Parameter, Year) %>%
-    filter(Parameter == "Flow") %>% # Only keeping one parameter to limit station redudancy
-    pull_station_number() %>%
-    hy_stations() %>%
-    select(STATION_NUMBER, STATION_NAME, DRAINAGE_AREA_GROSS, LATITUDE, LONGITUDE) %>% # Will remove the info on drainage area when I have the watersheds
-    st_as_sf(coords = c("LONGITUDE", "LATITUDE"), crs = 4326)
-  
-  ggplot() +
-    geom_sf(data = stns_1981_2019_list) # Plot point layer showing stations between 1981 and 2019
-  st_write(stns_1981_2019_list, "02_TRANSFORMED_DATA/HYDAT/stns_1981-2019_location.shp", append = F) # Write point shapefile
-  
+stns_1981_2019_list <- hy_annual_stats(start_year = 1981, end_year = 2019) %>%
+  select(STATION_NUMBER, Parameter, Year) %>%
+  filter(Parameter == "Flow") %>% # Only keeping one parameter to limit station redudancy
+  pull_station_number() %>%
+  hy_stations() %>%
+  select(STATION_NUMBER, STATION_NAME, DRAINAGE_AREA_GROSS, LATITUDE, LONGITUDE) %>% # Will remove the info on drainage area when I have the watersheds
+  st_as_sf(coords = c("LONGITUDE", "LATITUDE"), crs = 4326)
+
+ggplot() +
+  geom_sf(data = stns_1981_2019_list) # Plot point layer showing stations between 1981 and 2019
+st_write(stns_1981_2019_list, "02_TRANSFORMED_DATA/HYDAT/stns_1981-2019_location.shp", append = F) # Write point shapefile
+
 # This part extracts the location (LAT,LON) of gauges active in 1980 and still active (for at least one more year) between 1981 and 2019
-  stns_1980_no_geo <- st_drop_geometry(stns_1980_list)
-  stns_1981_2019_no_geo <- st_drop_geometry(stns_1981_2019_list)
-  stns_1980_2019 <- intersect(stns_1980_no_geo, stns_1981_2019_no_geo) %>% # returns 1765 gauges, the number of gauges that exist in 1980 and after
-    pull_station_number() %>%
-    hy_stations() %>%
-    select(STATION_NUMBER, STATION_NAME, DRAINAGE_AREA_GROSS, LATITUDE, LONGITUDE) %>% # Will remove the info on drainage area when I have the watersheds
-    st_as_sf(coords = c("LONGITUDE", "LATITUDE"), crs = 4326)
-  
-  diff_stns_1980_2019 <- setdiff(stns_1980_no_geo, st_drop_geometry(stns_1980_2019))# returns 57, meaning that 57 gauges were lost between 1980 and 2019. No direct use at the moment
-  
-  ggplot() +
-    geom_sf(data = stns_1980_2019, col = 'red') # Plot point layer showing the 1765 stations existing in 1980 and after
-  st_write(stns_1980_2019, "02_TRANSFORMED_DATA/HYDAT/stns_active_1980-2019_location.shp", append = F) # Write point shapefile
-  
+stns_1980_no_geo <- st_drop_geometry(stns_1980_list)
+stns_1981_2019_no_geo <- st_drop_geometry(stns_1981_2019_list)
+stns_1980_2019 <- intersect(stns_1980_no_geo, stns_1981_2019_no_geo) %>% # returns 1765 gauges, the number of gauges that exist in 1980 and after
+  pull_station_number() %>%
+  hy_stations() %>%
+  select(STATION_NUMBER, STATION_NAME, DRAINAGE_AREA_GROSS, LATITUDE, LONGITUDE) %>% # Will remove the info on drainage area when I have the watersheds
+  st_as_sf(coords = c("LONGITUDE", "LATITUDE"), crs = 4326)
+
+diff_stns_1980_2019 <- setdiff(stns_1980_no_geo, st_drop_geometry(stns_1980_2019)) # returns 57, meaning that 57 gauges were lost between 1980 and 2019. No direct use at the moment
+
+ggplot() +
+  geom_sf(data = stns_1980_2019, col = "red") # Plot point layer showing the 1765 stations existing in 1980 and after
+st_write(stns_1980_2019, "02_TRANSFORMED_DATA/HYDAT/stns_active_1980-2019_location.shp", append = F) # Write point shapefile
+
+# This part extracts watersheds from WCS shapefile
+stns_1980_2019_no_geo <- st_drop_geometry(stns_1980_2019)
+ws_1980_2019 <- watersheds_wcs %>%
+  inner_join(stns_1980_2019_no_geo, by = c("Station" = "STATION_NUMBER")) # Returns 817 objects (out of the 1765 identified, which means that 948 watersheds are missing)
+
+ggplot() +
+  geom_sf(data=ws_1980_2019, col = "red") # Plot selected watersheds for stations that exist between 1980 and 2019 only
+
 
 # Working -------------------
-  stns_list_area_join <- hy_annual_stats(start_year = 1981, end_year = 2019) %>%
-    select(STATION_NUMBER, Parameter, Year) %>%
-    filter(Parameter == "Flow") %>%
-    unique() %>%
-    left_join(stns_1981_2019_no_geo) 
-  
-  
-  yearly_drainage <- stns_list_area_join %>%
-    select(STATION_NUMBER, Year, DRAINAGE_AREA_GROSS) %>%
-    group_by(Year) %>%
-    summarise(sum_area = sum(DRAINAGE_AREA_GROSS, na.rm = T))
-  
-  ggplot() +
-    geom_bar(data = yearly_drainage, aes(x=Year, y=sum_area), stat="identity", fill="steelblue", width=0.7)
-  
+stns_list_area_join <- hy_annual_stats(start_year = 1981, end_year = 2019) %>%
+  select(STATION_NUMBER, Parameter, Year) %>%
+  filter(Parameter == "Flow") %>%
+  unique() %>%
+  left_join(stns_1981_2019_no_geo)
+
+
+yearly_drainage <- stns_list_area_join %>%
+  select(STATION_NUMBER, Year, DRAINAGE_AREA_GROSS) %>%
+  group_by(Year) %>%
+  summarise(sum_area = sum(DRAINAGE_AREA_GROSS, na.rm = T))
+
+ggplot() +
+  geom_bar(data = yearly_drainage, aes(x = Year, y = sum_area), stat = "identity", fill = "steelblue", width = 0.7)
